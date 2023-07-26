b1cb63b70e2a22551d420de04e2b1ef4f9517f2c
mateusseiboth <mateusseiboth@gmail.com>
2023-07-25 20:57:59 -0400

    upload inicial

---
+++ src/main/java/br/com/seisbot/vejus/VejusApplication.java
@@ -0,0 +1,13 @@
+package br.com.seisbot.vejus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VejusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VejusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/seisbot/vejus/VejusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seisbot.vejus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VejusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
